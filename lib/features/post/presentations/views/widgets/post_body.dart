e5b3fffcc1266a18b029339d4fe91780f81334f4
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-07-14 02:22:42 +0300

    added bloc provider and bloc builder to display post data.

--- lib/features/post/presentations/views/widgets/post_body.dart
+++ lib/features/post/presentations/views/widgets/post_body.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_task/core/widgets/custom_list_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_task/core/widgets/custom_list_tile.dart';
+import 'package:mobile_task/features/post/presentations/view_model/post/post_cubit.dart';
 
 class PostBody extends StatelessWidget {
   const PostBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomListView(title: 'Title', subTitle: 'Body', itemCount: 10);
+    return BlocBuilder<PostCubit, PostState>(
+      builder: (context, state) {
+        if (state is PostSuccess) {
+          return ListView.builder(
+            itemCount: state.posts.length,
+            itemBuilder: (context, index) {
+              final post = state.posts[index];
+              return Card(
+                child: CustomListTile(
+                  title: post.title ?? 'No Title',
+                  subTitle: post.body ?? 'No Body',
+                ),
+              );
+            },
+          );
+        } else if (state is PostFailure) {
+          return Center(child: Text(state.errMessage));
+        } else if (state is PostLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return const SizedBox.shrink();
+      },
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile_task/core/helpers/app_routes.dart';
 import 'package:mobile_task/core/services/service_locator.dart';
+import 'package:mobile_task/features/post/data/repos/post_repo_imp.dart';
+import 'package:mobile_task/features/post/presentations/view_model/post/post_cubit.dart';
 import 'package:mobile_task/features/user/data/repos/user_repo_imp.dart';
 import 'package:mobile_task/features/user/presentations/view_model/user/user_cubit.dart';
 
@@ -21,6 +23,10 @@ class MyApp extends StatelessWidget {
         BlocProvider(
           create:
               (context) => UserCubit(getIt.get<UserRepoImp>())..fetchUsers(),
+        ),
+        BlocProvider(
+          create:
+              (context) => PostCubit(getIt.get<PostRepoImp>())..fetchPosts(),
         ),
       ],
       child: MaterialApp.router(
