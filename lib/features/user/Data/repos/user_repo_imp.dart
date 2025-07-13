5281dc457641472170f1af33a4d9f2feb498f67f
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-07-13 16:52:56 +0300

    created user reop implementation.

--- lib/features/user/Data/repos/user_repo_imp.dart
+++ lib/features/user/Data/repos/user_repo_imp.dart
@@ -1,1 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:mobile_task/core/helpers/failure.dart';
+import 'package:mobile_task/features/user/data/models/user_model/user_model.dart';
+import 'package:mobile_task/features/user/data/repos/uesr_repo.dart';
 
+class UserRepoImp implements UesrRepo {
+  @override
+  Future<Either<Failure, List<UserModel>>> fetchUsers() {
+    throw UnimplementedError();
+  }
+}
