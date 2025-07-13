66e960f8b5888fc4dac1d6ced056c3996eb79074
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-07-14 02:16:39 +0300

    added post cubit.

--- lib/features/post/presentations/view_model/post/post_cubit.dart
+++ lib/features/post/presentations/view_model/post/post_cubit.dart
@@ -1,9 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mobile_task/features/post/data/models/post_model/post_model.dart';
+import 'package:mobile_task/features/post/data/repos/post_repo.dart';
 
 part 'post_state.dart';
 
 class PostCubit extends Cubit<PostState> {
-  PostCubit() : super(PostInitial());
+  PostCubit(this.postRepo) : super(PostInitial());
+  final PostRepo postRepo;
+  Future<void> fetchPosts() async {
+    emit(PostLoading());
+    final result = await postRepo.fetchPosts();
+    result.fold(
+      (failure) => emit(PostFailure(failure.errMessage)),
+      (posts) => emit(PostSuccess(posts)),
+    );
+  }
+
+  Future<void> fetchPostsByUserId(int userId) async {
+    emit(PostLoading());
+
+    final result = await postRepo.fetchPostsByUserId(userId);
+
+    result.fold(
+      (failure) => emit(PostFailure(failure.errMessage)),
+      (posts) => emit(PostSuccess(posts)),
+    );
+  }
 }
