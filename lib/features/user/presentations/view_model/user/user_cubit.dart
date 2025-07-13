711b9564aaf086a28bc201a84db00a5e7ae38705
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-07-13 18:19:36 +0300

    added user cubit.

--- lib/features/user/presentations/view_model/user/user_cubit.dart
+++ lib/features/user/presentations/view_model/user/user_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mobile_task/features/user/data/models/user_model/user_model.dart';
+import 'package:mobile_task/features/user/data/repos/uesr_repo.dart';
 
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit() : super(UserInitial());
+  UserCubit(this.uesrRepo) : super(UserInitial());
+  final UesrRepo uesrRepo;
+  Future<void> fetchUsers() async {
+    emit(UserLoading());
+    final result = await uesrRepo.fetchUsers();
+    result.fold(
+      (failure) => emit(UserFailure(failure.errMessage)),
+      (users) => emit(UserSuccess(users)),
+    );
+  }
 }
