d171cd3c4a572b6557aa674685fcb63a9fbe2f25
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-07-14 02:01:47 +0300

    creayed post model.

---
+++ lib/features/post/data/models/post_model/post_model.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+class PostModel extends Equatable {
+  final int? userId;
+  final int? id;
+  final String? title;
+  final String? body;
+
+  const PostModel({this.userId, this.id, this.title, this.body});
+
+  factory PostModel.fromJson(Map<String, dynamic> json) => PostModel(
+    userId: json['userId'] as int?,
+    id: json['id'] as int?,
+    title: json['title'] as String?,
+    body: json['body'] as String?,
+  );
+
+  Map<String, dynamic> toJson() => {
+    'userId': userId,
+    'id': id,
+    'title': title,
+    'body': body,
+  };
+
+  @override
+  List<Object?> get props => [userId, id, title, body];
+}
---
+++ lib/features/post/presentations/view_model/post/post_state.dart
@@ -0,0 +1,16 @@
+part of 'post_cubit.dart';
+
+sealed class PostState extends Equatable {
+  const PostState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class PostInitial extends PostState {}
+
+final class PostLoading extends PostState {}
+
+final class PostSuccess extends PostState {}
+
+final class PostFailure extends PostState {}
