eb50147eeb4d50058f386f361b0b436c9453fde0
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-07-13 16:49:53 +0300

    created user repo.

--- lib/features/user/Data/repos/uesr_repo.dart
+++ lib/features/user/Data/repos/uesr_repo.dart
@@ -1,5 +1,7 @@
 import 'package:dartz/dartz.dart';
+import 'package:mobile_task/core/helpers/failure.dart';
+import 'package:mobile_task/features/user/data/models/user_model/user_model.dart';
 
 abstract class UesrRepo {
-  Future<Either<Failure, List<Item>>> fetchBooks();
+  Future<Either<Failure, List<UserModel>>> fetchUsers();
 }
